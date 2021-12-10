ca19f51bbe600d8bfc818caf0aae6dabffb7a57a
palaych <palaychm@yandex.ru>
2021-12-11 03:43:08 +0900

    sweet: devicesettings: BootCompletedReceiver: import IntentFilter
    Change-Id: Iab3aa4fdb559864c22dd5a7b2c6df7385c0f0cb8

--- devicesettings/src/org/lineageos/settings/device/BootCompletedReceiver.java
+++ devicesettings/src/org/lineageos/settings/device/BootCompletedReceiver.java
@@ -21,6 +21,7 @@ import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.content.SharedPreferences;
 import android.os.Handler;
