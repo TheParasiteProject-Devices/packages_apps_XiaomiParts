78bbd4773ebd424eb21296911781d5789bfcde76
someone5678 <nemui3353@gmail.com>
2022-03-14 23:55:48 +0900

    sweet: devicesettings: set correct default refresh rate
    * import Constants
    * set REFRESH_STATE_DEFAULT as DEFAULT_REFRESH_RATE

--- devicesettings/src/org/lineageos/settings/device/refreshrate/RefreshUtils.java
+++ devicesettings/src/org/lineageos/settings/device/refreshrate/RefreshUtils.java
@@ -25,6 +25,8 @@ import android.view.Display;
 import android.provider.Settings;
 import androidx.preference.PreferenceManager;
 
+import org.lineageos.settings.device.Constants;
+
 public final class RefreshUtils {
 
     private static final String REFRESH_CONTROL = "refresh_control";
@@ -42,7 +44,7 @@ public final class RefreshUtils {
     protected static final int STATE_HIGH = 3;
     protected static final int STATE_EXTREME = 4;
 
-    private static final float REFRESH_STATE_DEFAULT = 60f;
+    private static final float REFRESH_STATE_DEFAULT = Constants.DEFAULT_REFRESH_RATE;
     private static final float REFRESH_STATE_LOW = 30f;
     private static final float REFRESH_STATE_MEDIUM = 60f;
     private static final float REFRESH_STATE_HIGH = 90f;
@@ -70,8 +72,8 @@ public final class RefreshUtils {
     }
 
    protected void getOldRate(){
-        defaultMaxRate = Settings.System.getFloat(mContext.getContentResolver(), KEY_PEAK_REFRESH_RATE, 60);
-        defaultMinRate = Settings.System.getFloat(mContext.getContentResolver(), KEY_MIN_REFRESH_RATE, 60);
+        defaultMaxRate = Settings.System.getFloat(mContext.getContentResolver(), KEY_PEAK_REFRESH_RATE, Constants.DEFAULT_REFRESH_RATE);
+        defaultMinRate = Settings.System.getFloat(mContext.getContentResolver(), KEY_MIN_REFRESH_RATE, Constants.DEFAULT_REFRESH_RATE);
     }
 
 
