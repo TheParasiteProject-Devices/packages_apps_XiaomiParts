6ab367a3727dd8a1bc5222914544842393f09b72
WSTxda <wstxda@gmail.com>
2022-03-31 13:34:18 -0300

    sweet: devicesettings: Use TOGGLEABLE_TILE in HBM and Dirac
    [someone5678]
    [1] remove hbm
    [2] adapt to current dirac code
    
    Original commit:
    https://github.com/PixelExperience-Devices/device_xiaomi_surya/commit/5f1a56ba53ad29f01f8f540a4a4ba31bc6e5cf59
    
    Co-authored-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- devicesettings/src/org/lineageos/settings/device/dirac/DiracTileService.java
+++ devicesettings/src/org/lineageos/settings/device/dirac/DiracTileService.java
@@ -9,33 +9,33 @@ import org.lineageos.settings.device.R;
 public class DiracTileService extends TileService {
 
     private DiracUtils mDiracUtils;
-    private Context mContext;
 
     @Override
     public void onStartListening() {
-        mContext = getApplicationContext();
-        mDiracUtils = DiracUtils.getInstance(mContext);
+        mDiracUtils = DiracUtils.getInstance(getApplicationContext());
 
-        updateTileContent(mDiracUtils.isDiracEnabled());
+        Tile tile = getQsTile();
+        if (mDiracUtils.isDiracEnabled()) {
+            tile.setState(Tile.STATE_ACTIVE);
+        } else {
+            tile.setState(Tile.STATE_INACTIVE);
+        }
+
+        tile.updateTile();
         super.onStartListening();
     }
 
     @Override
     public void onClick() {
-        boolean isEnabled = mDiracUtils.isDiracEnabled();
-        mDiracUtils.setMusic(!isEnabled);
-        updateTileContent(!isEnabled);
-        super.onClick();
-    }
-
-    private void updateTileContent(boolean isActive) {
         Tile tile = getQsTile();
-        String on = mContext.getResources().getString(R.string.dirac_tile_on);
-        String off = mContext.getResources().getString(R.string.dirac_tile_off);
-
-        tile.setState(isActive ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
-        tile.setContentDescription(isActive ? on : off);
-        tile.setSubtitle(isActive ? on : off);
+        if (mDiracUtils.isDiracEnabled()) {
+            mDiracUtils.setMusic(false);
+            tile.setState(Tile.STATE_INACTIVE);
+        } else {
+            mDiracUtils.setMusic(true);
+            tile.setState(Tile.STATE_ACTIVE);
+        }
         tile.updateTile();
+        super.onClick();
     }
 }
