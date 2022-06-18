e081badf6498af807cf702d83d838bfdae77bb13
Cyber Knight <cyberknight755@gmail.com>
2022-06-18 19:33:38 +0800

    sweet: devicesettings: KProfiles: Adapt to latest implementation
    - Latest implementation changes mode to kp_mode so let's adapt the codebase to it.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    
    Original commit:
    https://github.com/CannedShroud/android_packages_apps_KProfiles/commit/ac33e136043b6e2925ba258e957a08c2c567e9da
    
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- devicesettings/src/org/lineageos/settings/device/Constants.java
+++ devicesettings/src/org/lineageos/settings/device/Constants.java
@@ -18,5 +18,5 @@ public class Constants {
     public static final String KEY_KPROFILES_AUTO = "pref_kprofiles_auto";
     public static final String KPROFILES_AUTO_NODE = "/sys/module/kprofiles/parameters/auto_kprofiles";
     public static final String KEY_KPROFILES_MODES= "pref_kprofiles_modes";
-    public static final String KPROFILES_MODES_NODE = "/sys/module/kprofiles/parameters/mode";
+    public static final String KPROFILES_MODES_NODE = "/sys/module/kprofiles/parameters/kp_mode";
 }
