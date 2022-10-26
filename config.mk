49ee5b5c48ace474b21c6ff4057a7cc2bf471afa
someone5678 <nemui3353@gmail.com>
2022-10-26 23:30:22 +0900

    Parts: overlay: Palceholder

--- config.mk
+++ config.mk
@@ -9,6 +9,10 @@ $(call inherit-product, $(PARTS_PATH)/configs/devicesettings.mk)
 # Inherit from DeviceDoze config
 $(call inherit-product, $(PARTS_PATH)/configs/devicedoze.mk)
 
+# Overlays
+PRODUCT_PACKAGES += \
+    SettingsOverlayParts
+
 # Sepolicy
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(PARTS_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(PARTS_PATH)/sepolicy/public
