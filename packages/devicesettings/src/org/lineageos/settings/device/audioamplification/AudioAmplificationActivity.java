a5b00ac5e779c00de2770b4464c72a4e3717cdea
Bruno Martins <bgcngm@gmail.com>
2022-12-30 05:28:49 +0700

    Parts: Fix compilation for QPR1 merge
    Change-Id: I71ad7db85592ca18e3f0f8155bba5a472bfadbaa

--- packages/devicesettings/src/org/lineageos/settings/device/audioamplification/AudioAmplificationActivity.java
+++ packages/devicesettings/src/org/lineageos/settings/device/audioamplification/AudioAmplificationActivity.java
@@ -20,7 +20,7 @@ package org.lineageos.settings.device.audioamplification;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.collapsingtoolbar.R;
+import com.android.settingslib.widget.R;
 
 public class AudioAmplificationActivity extends CollapsingToolbarBaseActivity {
 
